5157c9009a0dd088f1eebf5a63602e3e0f895d0b
Natalie Kapitonova <Natalie.Kapitonova33o@ch.ibm.com>
2021-07-03 13:57:48 +0300

    update package

--- dist/core/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="react" />
-import { UIProps } from "./types-ui";
-import { CoreProps } from "./types-core";
-declare const DatePicker: ({ Input, CalendarOverlay }: UIProps) => (props: CoreProps) => JSX.Element;
-export default DatePicker;
--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import CorePicker from "./core";
-import * as UITypes from "./core/types-ui";
-import * as Types from "./core/types-core";
-export { CorePicker, UITypes, Types };
